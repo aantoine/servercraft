ee69dcd24ceec0893c9cd8e564a3447caca2ead5
aantoine <antoineagustin@gmail.com>
2016-10-16 12:49:05 -0300

    added args to runserver

--- runserver.py
+++ runserver.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from test.config import install_if_required
+from test.utils import get_args
 from test.models import create_tables
 from test.views import *
 
 if __name__ == '__main__':
+    args = get_args()
     create_tables()
     install_if_required()
     auth.load()
-    app.run(threaded=True)
+    app.run(threaded=True, debug=args.debug, host=args.host, port=args.port)
